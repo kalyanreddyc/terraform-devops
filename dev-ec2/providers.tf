a4933083f42af5556113bcabd073b3d65962f922
kalyanreddyc <139508882+kalyanreddyc@users.noreply.github.com>
2023-07-15 09:00:32 +0530

    Update providers.tf

--- dev-ec2/providers.tf
+++ dev-ec2/providers.tf
@@ -3,6 +3,6 @@ provider "aws" {
   profile = "kalyancisco"
 
   # Add your AWS access and secret keys or use a different authentication method
-  access_key = "AKIA6DU6NYUIGKDF3PP3"
-  secret_key = "S4FxJ2hMUxVTrifX4BKcYJHd7wnZcXAwBnqjjowj"
+  access_key = "#######"
+  secret_key = "##################"
 }
