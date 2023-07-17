4b038cf95e3a46ce18da34eb5ffb096deacca6d2
kalyanreddyc <139508882+kalyanreddyc@users.noreply.github.com>
2023-07-18 00:47:32 +0530

    Update providers.tf

--- vpc-dev-infra/providers.tf
+++ vpc-dev-infra/providers.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"  # Update with your desired region
 
   # Add your AWS access and secret keys or use a different authentication method
-  access_key = "AKIA6DU6NYUIGKDF3PP3"
-  secret_key = "S4FxJ2hMUxVTrifX4BKcYJHd7wnZcXAwBnqjjowj"
+  access_key = "#######"
+  secret_key = "#######"
 }
